50c80fc417e32093a521122bf94b308f9cbc45a8
ValerieBytes <140847810+ValerieBytes@users.noreply.github.com>
2024-01-25 08:45:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let stars = [];
+
 document.addEventListener('mousemove', (event) => {
     const star = document.createElement('div');
     star.className = 'trail';
@@ -5,8 +7,15 @@ document.addEventListener('mousemove', (event) => {
     star.style.top = `${event.pageY}px`;
     star.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewport="0 0 24 24" style="fill:black;"><path d="M12 .587l3.668 7.425 8.332 1.212-6.001 5.85 1.416 8.26L12 18.896l-7.415 3.938 1.416-8.26-6.001-5.85 8.332-1.212z"/></svg>`;
     document.body.appendChild(star);
+    stars.push(star);
+
+    if (stars.length > 5) { // Adjust the number of stars in the trail
+        let oldStar = stars.shift();
+        oldStar.remove();
+    }
 
     setTimeout(() => {
         star.remove();
-    }, 50); // Adjust the duration as needed
+        stars = stars.filter(item => item !== star);
+    }, 1000); // Stars disappear after 1 second
 });
