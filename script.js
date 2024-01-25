6c2f29d5cb7e9a3c87037bed801f046b0e6fb496
ValerieBytes <140847810+ValerieBytes@users.noreply.github.com>
2024-01-25 00:01:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener('mousemove', (event) => {
-    const trail = document.createElement('div');
-    trail.className = 'trail';
-    trail.style.left = `${event.pageX}px`;
-    trail.style.top = `${event.pageY}px`;
-    document.body.appendChild(trail);
+    const star = document.createElement('div');
+    star.className = 'trail';
+    star.style.left = `${event.pageX}px`;
+    star.style.top = `${event.pageY}px`;
+    star.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewport="0 0 24 24" style="fill:black;"><path d="M12 .587l3.668 7.425 8.332 1.212-6.001 5.85 1.416 8.26L12 18.896l-7.415 3.938 1.416-8.26-6.001-5.85 8.332-1.212z"/></svg>`;
+    document.body.appendChild(star);
 
     setTimeout(() => {
-        trail.remove();
-    }, 3000); // trail disappears after 500 milliseconds
+        star.remove();
+    }, 3000); // Adjust the duration as needed
 });
